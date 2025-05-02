5d006603a80bff12b52dfcdd2af1939c9099537d
Ryan Cheung <ry.adam.cheung@gmail.com>
2025-05-01 22:43:01 -0700

    easier language

--- balance_tree.py
+++ balance_tree.py
@@ -7,98 +7,130 @@ class Node:
         self.color = "red"  # Initially, all nodes are red
 
 class RedBlackTree:
-    ''' THIS IS THE PSUDOCODE VERSION OF THE RED BLACK TREE '''
+    ''' Red-Black Tree, Psudocode edition '''
     def __init__(self):
-        self.NIL = Node(None)  # Sentinel node representing null children
-        self.NIL.color = "black"
-        self.root = self.NIL
+        self.root = None
 
     def insert(self, data):
         new_node = Node(data)
-        self.root = self._insert_helper(new_node)
-        self._fix_insert(new_node)  # Correct the tree after insertion
-
-
-    def _insert_helper(self, node):
-        if self.root == self.NIL:
-            self.root = node
-            return node
-
+        if not self.root:
+            # First node becomes the root and is colored black
+            self.root = new_node
+            new_node.color = "black"
+            return
+            
+        # Regular binary search tree insertion
         current = self.root
         while True:
-            if node.data < current.data:
-                if current.left == self.NIL:
-                    current.left = node
-                    node.parent = current
-                    return node
-                else:
-                    current = current.left
+            if new_node.data < current.data:
+                if not current.left:
+                    current.left = new_node
+                    new_node.parent = current
+                    break
+                current = current.left
             else:
-                if current.right == self.NIL:
-                    current.right = node
-                    node.parent = current
-                    return node
-                else:
-                    current = current.right
+                if not current.right:
+                    current.right = new_node
+                    new_node.parent = current
+                    break
+                current = current.right
+                
+        # Fix the tree to maintain Red-Black properties
+        self._balance_after_insert(new_node)
 
-
-    def _fix_insert(self, node):
-        while node.parent and node.parent.color == "red":
+    def _balance_after_insert(self, node):
+        # Balance the tree after insertion
+        while node != self.root and node.parent.color == "red":
+            # If parent is a left child of grandparent
             if node.parent == node.parent.parent.left:
                 uncle = node.parent.parent.right
+                
+                # Case 1: Uncle is red
+                if uncle and uncle.color == "red":
+                    node.parent.color = "black"
+                    uncle.color = "black"
+                    node.parent.parent.color = "red"
+                    node = node.parent.parent
+                else:
+                    # Case 2: Node is a right child
+                    if node == node.parent.right:
+                        node = node.parent
+                        self._rotate_left(node)
+                    
+                    # Case 3: Node is a left child
+                    node.parent.color = "black"
+                    node.parent.parent.color = "red"
+                    self._rotate_right(node.parent.parent)
+            
+            # If parent is a right child of grandparent (parent.parent)
             else:
                 uncle = node.parent.parent.left
-
-            if uncle and uncle.color == "red":
+                
                 # Case 1: Uncle is red
-                node.parent.color = "black"
-                uncle.color = "black"
-                node.parent.parent.color = "red"
-                node = node.parent.parent  # Move up the tree
-            else:
-                # Case 2: Uncle is black
-                if node == node.parent.right: # node is right child
-                    node = node.parent
-                    if node == node.parent.left:
-                        # Case 2.1: Node is left child
-                        # Perform a left rotation on the parent
-                        self._rotate_left(node)
-                    break # Exit loop after rotation
+                if uncle and uncle.color == "red":
+                    node.parent.color = "black"
+                    uncle.color = "black"
+                    node.parent.parent.color = "red"
+                    node = node.parent.parent
                 else:
-                    break # Exit loop since uncle is black and node is left child
-
-            if node is None:
-                break
+                    # Case 2: Node is a left child
+                    if node == node.parent.left:
+                        node = node.parent
+                        self._rotate_right(node)
+                    
+                    # Case 3: Node is a right child
+                    node.parent.color = "black"
+                    node.parent.parent.color = "red"
+                    self._rotate_left(node.parent.parent)
+                    
         # Ensure root is always black
         self.root.color = "black"
-
-
 
     def _rotate_left(self, node):
         # Perform a left rotation at 'node'
         right_child = node.right
         node.right = right_child.left
-        if right_child.left is not None:
+        
+        if right_child.left:
             right_child.left.parent = node
-
+            
         right_child.parent = node.parent
-        if node.parent is None:
+        
+        if not node.parent:
             self.root = right_child
         elif node == node.parent.left:
             node.parent.left = right_child
         else:
             node.parent.right = right_child
-
+            
         right_child.left = node
         node.parent = right_child
 
-
+    def _rotate_right(self, node):
+        # Perform a right rotation at 'node'
+        left_child = node.left
+        node.left = left_child.right
+        
+        if left_child.right:
+            left_child.right.parent = node
+            
+        left_child.parent = node.parent
+        
+        if not node.parent:
+            self.root = left_child
+        elif node == node.parent.right:
+            node.parent.right = left_child
+        else:
+            node.parent.left = left_child
+            
+        left_child.right = node
+        node.parent = left_child
 
     def print_tree(self):
         self._print_tree_helper(self.root)
 
     def _print_tree_helper(self, node):
-        if node is None or node == self.NIL:
+        if not node:
             return
         print(f"Node: {node.data}, Color: {node.color}")
         self._print_tree_helper(node.left)
@@ -112,6 +144,5 @@ tree.insert(20)
 tree.insert(30)
 tree.insert(40)
 tree.insert(50)
-
 
 tree.print_tree()
